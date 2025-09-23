5d2c89a1817d0420a860fbb8f3c25d81d5bc6ac3
Laxen <patrik.lax@live.se>
2025-09-23 14:22:37 +0200

    add modal when washing

--- app/modal.tsx
+++ app/modal.tsx
@@ -1,29 +1,73 @@
-import { Link } from 'expo-router';
-import { StyleSheet } from 'react-native';
-
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { Text } from "@react-navigation/elements";
+import * as Haptics from "expo-haptics";
+import { router } from "expo-router";
+import { Pressable, StyleSheet, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function ModalScreen() {
+  const insets = useSafeAreaInsets();
+
+  const handleCancel = () => {
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+    router.push("/");
+  };
+
   return (
-    <ThemedView style={styles.container}>
-      <ThemedText type="title">This is a modal</ThemedText>
-      <Link href="/" dismissTo style={styles.link}>
-        <ThemedText type="link">Go to home screen</ThemedText>
-      </Link>
-    </ThemedView>
+    <View style={[styles.container, { paddingVertical: insets.top + 5 }]}>
+      <Text style={styles.header}>Du kan nu börja tvätta</Text>
+      <View style={styles.carwashIcon}>
+        <MaterialCommunityIcons name="car-wash" size={200} color="#fff" />
+      </View>
+      <Pressable
+        onPress={() => {handleCancel()}}
+        style={({ pressed }) => [
+          styles.pressable,
+          pressed && styles.pressablePressed,
+        ]}
+      >
+        <Text style={styles.text}>Avsluta</Text>
+      </Pressable>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
     padding: 20,
+    backgroundColor: "#9ba4f5",
   },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
+  header: {
+    fontSize: 30,
+    marginTop: 30,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+  carwashIcon: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#ffffff",
+  },
+  pressable: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: "auto",
+    marginBottom: 30,
+    padding: 10,
+    borderRadius: 5,
+    minWidth: 270,
+    backgroundColor: "#ff3b30",
+  },
+  pressablePressed: {
+    opacity: 0.8,
+    transform: [{ scale: 0.96 }],
   },
 });
